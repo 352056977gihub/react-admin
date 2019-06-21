fb49adfa5f3665c9b0c4efb1bd5f8db472c1c447
wxx123 <352056977@qq.com>
2019-06-21 12:18:16 +0800

    添加react-router和antd

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import React, {Component} from 'react';
+import {Switch,Route,Redirect} from 'react-router-dom'
+
+import Login from './pages/login'
+import Main from './pages/main'
 
 export default class App extends Component {
     render() {
         return <div>
-            App
+            <Switch>
+                <Route path='/login' component={Login}/>
+                <Route path='/' component={Main} />
+                <Redirect to='/login' />
+            </Switch>
         </div>;
     }
 }
